0a84c16a6a7d8dd252e22fc573761ed3ba9966e8
kim-shun <kimura1417080@gmail.com>
2022-12-22 20:30:50 +0900

    挿入ソート

---
+++ SampleProject/src/main/java/example/InsertSortAnswer.java
@@ -0,0 +1,45 @@
+package example;
+
+import java.util.Scanner;
+
+public class InsertSortAnswer {
+
+	static void print(int a[], int n) {
+        for (int i = 0; i < n; i++) {
+            if (i > 0) System.out.print(" ");
+            System.out.print(a[i]);
+        }
+        System.out.println();
+    }
+
+    static void insertion_sort(int a[], int n) {
+        for (int i = 1; i < n; i++) {
+            int x = a[i];
+            int j = i - 1;
+
+            while (j >= 0 && a[j] > x) {
+                a[j + 1] = a[j];
+                j--;
+            }
+
+            a[j + 1] = x;
+
+            print(a, n);
+        }
+    }
+
+    public static void main(String[] args) {
+    	@SuppressWarnings("resource")
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        insertion_sort(a, n);
+    }
+
+}
